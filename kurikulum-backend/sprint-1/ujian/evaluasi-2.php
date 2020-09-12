bd333a60ac610da815315057678090c8bb95ca45
AkhiFarid20 <akunit1453@gmail.com>
2020-09-12 12:57:39 +0700

    sprint-1

---
+++ kurikulum-backend/sprint-1/ujian/evaluasi-2.php
@@ -0,0 +1,5 @@
+<?php
+
+function selectionValue($collectionValue); {
+    $int = array 
+}
---
+++ kurikulum-backend/sprint-1/ujian/evaluasi-3.php
@@ -0,0 +1,20 @@
+<?php
+
+$students = [
+    [
+      'name' => ('ali');
+      'nik'  => ('T005');
+      'jurusan' => ('programmer');
+      'divisi' => ('Backend');
+
+      'name' => ('aldi');
+      'nik'  => ('T006');
+      'jurusan' => ('programmer');
+      'divisi' => ('Mobile');
+    
+    ]
+  ];
+  
+  function jurusanbackend(array = $students){
+      foreach ($students as $key => $value);
+  }
